f0d6ed06a3e3868b1060d45a358ad5a8872991fc
blessed wesonga <blessedwesonga@gmail.com>
2024-09-25 21:42:46 +0300

    add phase-1-control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 400 && distance <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance > 2000 && distance <= 2500) {
+
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+  
+
+function ternaryCheckCity(destination){
+  // Write your code here!
+  return destination === "NYC" ? "Ok, sounds good." : "No go.";
+  return destination === "New York" ? "Welcome to the Big Apple!" : 
+  destination === "Los Angeles" ? "Welcome to the City of Angels!" :
+  destination === "Chicago" ? "Welcome to the Windy City!" : 
+ "Destination not recogninized"
 }
 
-function ternaryCheckCity(){
+
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":  
+      return "Thank you.";
+    default:
+      return "Bye.";  
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
